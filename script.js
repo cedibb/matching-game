199d9cf919c38b6592363dee87159398fdddb06c
Ed Dibb <cedibb@outlook.com>
2019-09-03 20:55:17 -0300

    Some refactoring done

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
-var numberOfFaces = 5;
-var level = 1;
+let letnumberOfSmiles = 5;
+let level = 1;
+let theLeftSide = document.getElementById("leftSide");
+let theRightSide = document.getElementById("rightSide");
+let theBody = document.querySelector("body");
+
 document.getElementById("level").innerHTML = level;
-var theLeftSide = document.getElementById("leftSide");
-var theRightSide = document.getElementById("rightSide");
-var theBody = document.getElementsByTagName("body")[0];
+
+function delete_all_children () {
+    while (theLeftSide.lastChild)  //Removes the left faces.
+        theLeftSide.removeChild(theLeftSide.lastChild);
+    while (theRightSide.lastChild) //Removes the right faces.
+        theRightSide.removeChild(theRightSide.lastChild);
+}
+
 function generateFaces () {
-    for (var i = 0; i < numberOfFaces; i++) {
-        var img = document.createElement("img");
-        img.src = "http://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png"
+
+    for (let i = 0; i < letnumberOfSmiles; i++) {
+        let img = document.createElement("img");
+        img.src = "smile.png"
         img.style.top = "" + Math.floor((Math.random() * 401)) + "px";  //Creates a random top margin between 0 and 400.
         img.style.left = "" + Math.floor((Math.random() * 401)) + "px"; //Creates a random left margin between 0 and 400.
         theLeftSide.appendChild(img);
     }
+    
     leftSideImages = theLeftSide.cloneNode(true);
     leftSideImages.removeChild(leftSideImages.lastChild);
     theRightSide.appendChild(leftSideImages);
-    theLeftSide.lastChild.onclick = function nextLevel (event) {
+
+    theLeftSide.lastChild.addEventListener('click', (e) => {
         delete_all_children();
-        event.stopPropagation();
-        numberOfFaces += 5;
+        e.stopPropagation();
+        letnumberOfSmiles += 5;
         generateFaces();
         level++;
         document.getElementById("level").innerHTML = level;
-    };
+    });
+
     theBody.onclick = function gameOver() {
-        var tryAgain = confirm("Game Over! Want to try again?");
+        let tryAgain = confirm("Game Over! Want to try again?");
         if (tryAgain) {
-            numberOfFaces = 5;
+            letnumberOfSmiles = 5;
             delete_all_children();
             generateFaces();
             level = 1;
@@ -37,10 +50,5 @@ function generateFaces () {
             theLeftSide.lastChild.onclick = null;
         }
     };
-    function delete_all_children () {
-        while (theLeftSide.lastChild)  //Removes the left faces.
-            theLeftSide.removeChild(theLeftSide.lastChild);
-        while (theRightSide.lastChild) //Removes the right faces.
-            theRightSide.removeChild(theRightSide.lastChild);
-    }
+
 }
